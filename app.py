522551a2011230f36615643d6e595ff1cef0811e
Ale4ero <ale.forero10@gmail.com>
2024-09-12 14:09:26 -0400

    added manual scan feature and new menu. along with some new log info

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from datetime import datetime
 
 
 class FileMoverHandler(FileSystemEventHandler):
+
+
     def __init__(self):
         self.path = "C:/Users/alefo/Downloads"
         self.pptDes = "C:/Users/alefo/Downloads/Downloaded PPT"
@@ -20,32 +22,36 @@ class FileMoverHandler(FileSystemEventHandler):
         self.executeDes = "C:/Users/alefo/Downloads/Downloaded Execute"
         self.excelDes = "C:/Users/alefo/Downloads/Downloaded Excel"
 
+
+
+    # method that moved the files to appropriate folder location
     def move_file(self, filePath, dest, file):
         base, extension = os.path.splitext(filePath)
         destPath = os.path.join(dest, os.path.basename(filePath))
 
         if os.path.exists(destPath):
-            print("Path: "+ destPath + " already exists!")
+            print("Path: "+ destPath + " already exists!\n")
             timestamp = datetime.now().strftime('%y_%m_%d_%I_%M_%S')
             # destPath = os.path.join(dest, f"{os.path.basename(base)}_{timestamp}{extension}")
             destPath = dest+ "/"+ f"{os.path.basename(base)}_{timestamp}{extension}"
             print("Path changed to: "+ destPath )
 
-        print('MOVING: "' + file +'" --> "'+destPath+'"')
+        print('--MOVING: "' + file +'" --> "'+destPath+'"\n')
         shutil.move(filePath, destPath)
-    
-    def on_created(self, event):
-        if event.is_directory:
-            return
-        
-        filePath = event.src_path
 
-        file = os.path.basename(filePath).lower()
-        allFiles = os.listdir(path)
+
+    # method that scans the folder and identifies files by their extension
+    def scan_folder(self):
+        
+        print("SCANNING...\n")
+
+        # file = os.path.basename(filePath).lower()
+        allFiles = os.listdir(self.path)
         other = []
 
         for file in allFiles:
-            filePath = path+"/"+file
+            # filePath = path+"/"+file
+            filePath = os.path.join(self.path, file)
             file = file.lower()
 
             if file.endswith('.png') or file.endswith(".jpg") or file.endswith('.heic') or file.endswith('.gif'):
@@ -76,11 +82,31 @@ class FileMoverHandler(FileSystemEventHandler):
                 if ("downloaded" not in file) and (".tmp" not in file):
                     # print(file)
                     other.append(file)
-        
-        if other: 
-            print("THESE FILES ARE NOT ORGANIZED:")
-            print(other)
 
+        print("\nSCAN COMPLETE.\n")
+
+        if other: 
+            print("\nTHESE FILES ARE NOT ORGANIZED:")
+            print(other)
+            print("\n\n")
+
+        
+
+
+
+
+    # This method is called whenever a new file is created in the monitores directory
+    def on_created(self, event):    
+        if event.is_directory:
+            return
+        self.scan_folder()
+
+
+# MAIN BLOCK
+        # Initializes the Observer
+        # assigns the FileMoverHandler to the Observer 
+        # starts monitoring the downloads directory 
+        # Script keeps running until interrupted manually 
 if __name__ == "__main__":
     path = "C:/Users/alefo/Downloads"
     event_handler = FileMoverHandler()
@@ -89,7 +115,24 @@ if __name__ == "__main__":
     observer.start()
 
     try:
+        print("----------------------------------------------------------------------------------------")
+        print("\t\t\tWELCOME TO FILE ORGANIZER\n")
+        print("Script will monitor the downloads folder, organizing files to desired folder locations.")
+        print("----------------------------------------------------------------------------------------")
+        print("\nScript will scan automatically when new file is added or you can scan manually.")
+        print("Type 'run' to manually scan the folder, or 'exit' to quit.\n\n")
         while True:
+            user_input = input("> ").strip().lower()
+            if user_input == "run":
+                event_handler.scan_folder()
+            elif user_input == "exit":
+                print ("Exiting...")
+                print ("\n")
+                observer.stop()
+                break
+            else:
+                print("Invalid Input.\n Type 'run' to scan or 'exit' to quit.\n")
+
             pass
     except KeyboardInterrupt:
         observer.stop()
